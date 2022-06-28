9604ca2ab9e041d516def40b3a8511a1a7f669ec
alina1412 <alina1412@mail.ru>
2022-06-28 10:47:30 +0300

    debug sqlite setting

--- geo_app/database.py
+++ geo_app/database.py
@@ -4,13 +4,19 @@ from sqlalchemy.orm import sessionmaker
 
 from .settings import Settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = Settings().DATABASE_URL
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread": False}  # only for sqlite
-)
+if Settings().DEBUG:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, 
+        connect_args={"check_same_thread": False}  # only for sqlite
+    )
+else:
+    SQLALCHEMY_DATABASE_URL = Settings().DATABASE_URL
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, 
+        # connect_args={"check_same_thread": False}  # only for sqlite
+    )
 
 SessionLocal = sessionmaker(
     bind=engine,
